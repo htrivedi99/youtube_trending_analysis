776e22b9d0b0dfb6ac92d54202b10875f7fe75c1
Het Trivedi <Het@Chs-MacBook-Pro.local>
2018-10-28 12:40:57 -0700

    Added France Data

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ server = app.server
 
 
 # CA_data = pd.read_csv('data/CAvideos.csv') #Canada
-# FR_data = pd.read_csv('data/FRvideos.csv') #France
+FR_data = pd.read_csv('data/FRvideos.csv') #France
 # GB_data = pd.read_csv('data/GBvideos.csv') #UK
 # DE_data = pd.read_csv('data/DEvideos.csv') #Germany
 US_data = pd.read_csv('data/USvideos.csv') #US
@@ -38,12 +38,12 @@ def add_category_name():
         category_dict[int(category["id"])] = category["snippet"]["title"]
     US_data['category_name'] = US_data["category_id"].map(category_dict)
 
-    # with open('data/FR_category_id.json') as f:
-    #     categories = json.load(f)["items"]
-    # category_dict = {}
-    # for category in categories:
-    #     category_dict[int(category["id"])] = category["snippet"]["title"]
-    # FR_data['category_name'] = FR_data["category_id"].map(category_dict)
+    with open('data/FR_category_id.json') as f:
+        categories = json.load(f)["items"]
+    category_dict = {}
+    for category in categories:
+        category_dict[int(category["id"])] = category["snippet"]["title"]
+    FR_data['category_name'] = FR_data["category_id"].map(category_dict)
     #
     # with open('data/DE_category_id.json') as f:
     #     categories = json.load(f)["items"]
@@ -62,13 +62,13 @@ def add_category_name():
 add_category_name()
 
 US_data['region'] = 'US'
-# FR_data['region'] = 'FR'
+FR_data['region'] = 'FR'
 # GB_data['region'] = 'GB'
 # DE_data['region'] = 'DE'
 # CA_data['region'] = 'CA'
 
 #df = pd.concat([CA_data, FR_data, GB_data, DE_data, US_data])
-df = pd.concat([US_data])
+df = pd.concat([US_data, FR_data])
 
 df['year_published'] = df['publish_time'].astype(str).str[:4]
 df['year_published'] = df['year_published'].astype('int')
@@ -114,7 +114,7 @@ app.layout = html.Div([
                 {'label':'United States', 'value':'US'},
                 # {'label':'Canada', 'value':'CA'},
                 # {'label':'United Kingdom', 'value':'GB'},
-                # {'label':'France', 'value':'FR'},
+                {'label':'France', 'value':'FR'},
                 # {'label':'Germany', 'value':'DE'}
                 ],
                 value=['US'],
